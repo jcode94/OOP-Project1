5dcade1df6d6863ffedad6d28c41fc9e89cf3499
Justice Smith <59818423+jcode94@users.noreply.github.com>
2021-02-11 17:51:16 -0500

    Update Menu.java
    added initial choice lines

--- Menu.java
+++ Menu.java
@@ -1,7 +1,10 @@
 public class Menu {
-	public static void main(String[] args) {
-	// This will contain the highest-level calls for 
-	// printing the menu options and receiving user input
-	
+	public static void displayMenu() {
+		System.out.println("1- Enter information for two students");
+		System.out.println("2- Print tuition invoice");
+		System.out.println("3- Enter faculty member information");
+		System.out.println("4- Print faculty member information");
+		System.out.println("5- Enter staff member information");
+		System.out.println("6- Print staff member information");
 	}
 }
