aad20d107e4535f4e702158a935514aa6f521368
tovat12 <78883363+tovat12@users.noreply.github.com>
2021-02-12 20:26:23 -0500

    Update Menu.java
    added comment

--- Menu.java
+++ Menu.java
@@ -1,7 +1,7 @@
 public class Menu {
 	public static void displayMenu() {
 		System.out.println("1- Enter information for two students");
-		System.out.println("2- Print tuition invoice");
+		System.out.println("2- Print tuition invoice");//hellooo
 		System.out.println("3- Enter faculty member information");
 		System.out.println("4- Print faculty member information");
 		System.out.println("5- Enter staff member information");
